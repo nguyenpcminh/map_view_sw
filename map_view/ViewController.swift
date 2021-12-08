e0f607a8607f6df2ef88094bb5e766004cf195cb
NPC Minh <45456587+nguyenpcminh@users.noreply.github.com>
2021-12-08 14:57:37 +0700

    Initial Commit

---
+++ map_view/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  map_view
+//
+//  Created by Nguyen Pham Cong Minh on 08/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ map_viewUITests/map_viewUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  map_viewUITestsLaunchTests.swift
+//  map_viewUITests
+//
+//  Created by Nguyen Pham Cong Minh on 08/12/2021.
+//
+
+import XCTest
+
+class map_viewUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
